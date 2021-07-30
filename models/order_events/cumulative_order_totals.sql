77bdd1604bc9562adce840b20f72a807560c062c
suziecarlson.90@gmail.com <suziecarlson.90@gmail.com>
2021-07-30 14:07:37 +0000

    .

--- models/order_events/cumulative_order_totals.sql
+++ models/order_events/cumulative_order_totals.sql
@@ -1,7 +1,7 @@
-select date_trunc('day',created_date) as created_date,
-        count(distinct order_id) as total_orders,
+select date_trunc('month',created_date) as created_month,
+        count(distinct order_id) as total_monthly_orders,
         count(distinct customer_id) as unique_customers,
-        sum(revenue) OVER (ORDER BY created_date) as cumulative_daily_revenue
+        sum(revenue) OVER (ORDER BY date_trunc('month',created_date)) as cumulative_monthly_revenue
         from {{ ref('orders') }}
         group by 1
         order by 1 asc
